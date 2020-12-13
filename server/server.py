7ee7c1b1e2a9130985cfbec2bf010efbd60e6e11
StukalinaZina <46031465+StukalinaZina@users.noreply.github.com>
2020-12-13 23:31:34 +0300

    Add files via upload

---
+++ server/server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template, make_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+    
+@app.route("/setcookie", methods = ["POST", "GET"])
+def setcookie():
+    if request.method == "POST":
+        user = request.form["User"]
+    resp = make_response(render_template("index.html"))
+    resp.set_cookie("userID", user)
+    return resp
+
+@app.route("/getcookie")
+def getcookie():
+    return render_template("last.html")
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/finish")
+def finish():
+    name = request.cookies.get("userID")
+    return "<h1> Good job, " + name + "</h1>"
+
+if __name__ == "__main__":
+    app.run()
